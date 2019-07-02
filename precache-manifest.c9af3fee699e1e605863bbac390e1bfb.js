ada3dbee6b5a23ce50fc6dc58f5294ea03120626
Yisheng <yisheng@Yishengs-MacBook-Pro.local>
2019-07-03 03:02:36 +0800

    Updates

---
+++ precache-manifest.c9af3fee699e1e605863bbac390e1bfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e012506127b63b971464a701f30d1e",
+    "url": "/l5r-informant/index.html"
+  },
+  {
+    "revision": "6ffea4b50761d546884a",
+    "url": "/l5r-informant/static/css/main.001355a1.chunk.css"
+  },
+  {
+    "revision": "bc2928af34cbfc2bd341",
+    "url": "/l5r-informant/static/js/2.1431d35d.chunk.js"
+  },
+  {
+    "revision": "6ffea4b50761d546884a",
+    "url": "/l5r-informant/static/js/main.52f488c0.chunk.js"
+  },
+  {
+    "revision": "21250fffb11985f64924",
+    "url": "/l5r-informant/static/js/runtime~main.ed8f595f.js"
+  }
+]);
